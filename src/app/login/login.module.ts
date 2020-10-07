943499d7fb68c5a4957c59702a5b79ffa3e85138
shiva-github <srp.sanju74482@gmail.com>
2020-10-08 00:04:06 +0530

    initial commit

---
+++ src/app/login/login.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+
+
+
+
+import {MatInputModule} from '@angular/material/input';
+
+const routes: Routes = [
+  {
+    path: '', children: [
+      { path: '', component: LoginComponent }
+    ]
+  }
+];
+
+
+@NgModule({
+  declarations: [LoginComponent],
+  imports: [
+    RouterModule.forChild(routes),
+    CommonModule,
+    MatCardModule,
+    MatInputModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule
+  ]
+})
+export class LoginModule { }
