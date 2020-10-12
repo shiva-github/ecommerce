c0d79af85d055dada9faf8b2195f456f0001862a
shiva-github <srp.sanju74482@gmail.com>
2020-10-12 15:25:56 +0530

    login tabs

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -5,6 +5,7 @@ import { LoginComponent } from './login.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatTabsModule} from '@angular/material/tabs';
 
 
 
@@ -29,6 +30,7 @@ const routes: Routes = [
     MatCardModule,
     MatInputModule,
     MatInputModule,
+    MatTabsModule,
     MatButtonModule,
     MatIconModule
   ]
